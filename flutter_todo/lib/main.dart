05189674a234cb3d602b72140e9a95b57f6f1b9a
songye38 <ddev0308@gmail.com>
2025-06-13 14:25:47 +0900

    ADD 새로운 todo 추가 기능 구현

--- flutter_todo/lib/main.dart
+++ flutter_todo/lib/main.dart
@@ -4,20 +4,71 @@ import 'widgets/todo_item_widget.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  final todos = [
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<TodoItem> todos = [
     TodoItem(id: 1, text: '첫 번째 할 일'),
     TodoItem(id: 2, text: '두 번째 할 일'),
     TodoItem(id: 3, text: '세 번째 할 일'),
   ];
+
+  final TextEditingController _controller = TextEditingController();
+  int _nextId = 4;
+
+  void _addTodo() {
+    final text = _controller.text.trim();
+    if (text.isEmpty) return;
+
+    setState(() {
+      todos.add(TodoItem(id: _nextId++, text: text));
+      _controller.clear();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Todo App')),
-        body: ListView(
-          children: todos.map((todo) => TodoItemWidget(todo: todo)).toList(),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                children: todos
+                    .map((todo) => TodoItemWidget(todo: todo))
+                    .toList(),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _controller,
+                      decoration: InputDecoration(
+                        labelText: '새 할 일 입력',
+                        border: OutlineInputBorder(),
+                      ),
+                      onSubmitted: (_) => _addTodo(),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  ElevatedButton(onPressed: _addTodo, child: Text('추가')),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
